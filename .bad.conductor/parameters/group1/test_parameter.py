c75cba8f0e7f33bc89070061fb96ecf08f30fe95
srgang <yesrgang@gmail.com>
2018-10-31 15:01:18 -0600

    .

--- .bad.conductor/parameters/group1/test_parameter.py
+++
@@ -1,6 +0,0 @@
-from conductor.parameter import ConductorParameter
-
-class TestParameter(ConductorParameter):
-    """ default behavior """
-
-Parameter = TestParameter
--- .bad.sequencer/devices/yesr_sequencer_board/exceptions.py
+++
@@ -1,8 +0,0 @@
-import traceback
-
-class TimeOutOfBoundsError(Exception):
-    def __init__(self, time, ticks, clk):
-        traceback.print_exc()
-        message = 'time {} [s] corresponds to {} {} [Hz] clock cycles'.format(time, ticks, clk)
-        super(TimeOutOfBoundsError, self).__init__(message)
-
