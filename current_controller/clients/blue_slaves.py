3ca7db052f16cddcd6ee5ab0d5711e5060706fe2
ross hutson <rosshutson@gmail.com>
2018-11-02 02:24:44 -0600

    add blue_slaves client

---
+++ current_controller/clients/blue_slaves.py
@@ -0,0 +1,34 @@
+from current_controller.clients.default import MultipleClientContainer
+from current_controller.clients.default import CurrentControllerClient
+
+class Client3D(CurrentControllerClient):
+    name = '3d'
+    servername = 'current_controller'
+    update_time = 100
+
+class Client2D(CurrentControllerClient):
+    name = '2d'
+    servername = 'current_controller'
+    update_time = 100
+
+class ClientZS(CurrentControllerClient):
+    name = 'zs'
+    servername = 'current_controller'
+    update_time = 100
+
+class MyClientContainer(MultipleClientContainer):
+    qt_style = 'Gtk+'
+
+if __name__ == '__main__':
+    from PyQt4 import QtGui
+    app = QtGui.QApplication([])
+    from client_tools import qt4reactor 
+    qt4reactor.install()
+    from twisted.internet import reactor
+
+    channels = ['3d', '2d', 'zs']
+    widgets = [Client3D(reactor), Client2D(reactor), ClientZS(reactor)]
+    widget = MyClientContainer(widgets, reactor)
+    app.setStyle(QtGui.QStyleFactory.create(widget.qt_style))
+    widget.show()
+    reactor.run()
