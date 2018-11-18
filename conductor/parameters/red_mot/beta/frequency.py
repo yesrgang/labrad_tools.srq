9c0d73ef40cf3484720491c01605aa79cee3929e
SrQ <yesrgang@gmail.com>
2018-11-18 01:37:34 -0700

    red mot frequency monitoring

---
+++ conductor/parameters/red_mot/beta/frequency.py
@@ -0,0 +1,21 @@
+import json
+
+from conductor.parameter import ConductorParameter
+
+
+class Frequency(ConductorParameter):
+    priority = 1
+    autostart = True
+    call_in_thread = True
+    rf_devicename = 'beta'
+
+    def initialize(self, config):
+        super(Frequency, self).initialize(config)
+        self.connect_to_labrad()
+    
+    def update(self):
+        request = {self.rf_devicename: None}
+        response_json = self.cxn.rf.frequencies(json.dumps(request))
+        self.value = json.loads(response_json)
+
+Parameter = Frequency
