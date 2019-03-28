caaf5d916a2512bfba9c55c609d7172be55769ad
srgang <yesrgang@gmail.com>
2019-03-28 16:38:00 -0600

    add 3d2 controller

---
+++ current_controller3/devices/_3d2.py
@@ -0,0 +1,12 @@
+from current_controller3.devices.ldc80 import LDC80, LDC80Proxy
+
+class Device(LDC80):
+    autostart = False
+    gpib_address = 'GPIB0::9::INSTR'
+
+    pro8_slot = 8
+    default_current = 0.1500
+    current_range = [0.0, 0.155]
+
+class DeviceProxy(Device, LDC80Proxy):
+    gpib_servername = 'yesr20_visa2'
