351a124b211bb8736518d06a430f89745173315b
SrQ <yesrgang@gmail.com>
2019-04-18 02:06:15 -0600

    .

--- sequencer/devices/h.py
+++ sequencer/devices/h.py
@@ -5,22 +5,20 @@ class H(YeSrAnalogBoard):
     autostart = True
     
     conductor_servername = 'conductor'
-#    okfpga_servername = 'daisy_ok'
-#    okfpga_interface = '1541000D38'
-    okfpga_servername = 'yesr20_ok'
-    okfpga_interface = '1541000D38'
+    ok_servername = 'yesr5_ok'
+    ok_interface = '1541000D38'
 
     is_master = False
 
     channels = [
-        YeSrAnalogChannel(loc=0, name='DACG00', mode='auto', manual_output=0.0),
-        YeSrAnalogChannel(loc=1, name='Clock AOM Phase', mode='auto', manual_output=1.0),
-        YeSrAnalogChannel(loc=2, name='DACG02', mode='auto', manual_output=0.0),
-        YeSrAnalogChannel(loc=3, name='813H2 pzt H', mode='auto', manual_output=0.0),
-        YeSrAnalogChannel(loc=4, name='813H2 pzt V', mode='auto', manual_output=0.0),
-        YeSrAnalogChannel(loc=5, name='DACG05', mode='auto', manual_output=0.0),
-        YeSrAnalogChannel(loc=6, name='DACG06', mode='auto', manual_output=0.0),
-        YeSrAnalogChannel(loc=7, name='DACG07', mode='auto', manual_output=0.0),
+        YeSrAnalogChannel(loc=0, name='DACH00', mode='auto', manual_output=0.0),
+        YeSrAnalogChannel(loc=1, name='DACH01', mode='auto', manual_output=0.0),
+        YeSrAnalogChannel(loc=2, name='DACH02', mode='auto', manual_output=0.0),
+        YeSrAnalogChannel(loc=3, name='DACH03', mode='auto', manual_output=0.0),
+        YeSrAnalogChannel(loc=4, name='DACH04', mode='auto', manual_output=0.0),
+        YeSrAnalogChannel(loc=5, name='DACH05', mode='auto', manual_output=0.0),
+        YeSrAnalogChannel(loc=6, name='DACH06', mode='auto', manual_output=0.0),
+        YeSrAnalogChannel(loc=7, name='DACH07', mode='auto', manual_output=0.0),
         ]
 
-#Device = H
+Device = H
