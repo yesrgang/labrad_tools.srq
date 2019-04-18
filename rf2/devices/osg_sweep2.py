8fa7893fe93215d2489cff4435b736b02b4af77c
ross hutson <rosshutson@gmail.com>
2019-04-18 01:15:22 -0600

    .

--- rf2/devices/osg_sweep2.py
+++ rf2/devices/osg_sweep2.py
@@ -1,14 +1,14 @@
-from rf2.devices.dg4xxx import DG4xxx, DG4xxxProxy
+from rf2.devices.dg4000 import DG4000, DG4000Proxy
 
-class Device(DG4xxx):
+class Device(DG4000):
     _amplitude_range = (0.0, 5.0)
     _amplitude_units = 'V'
     _frequency_range = (0, 160e6)
     _source = 2
     _vxi11_address = '192.168.1.40'
 
-class DeviceProxy(Device, DG4xxxProxy):
+class DeviceProxy(Device, DG4000Proxy):
     _vxi11_servername = 'yesr10_vxi11'
     
     def __init__(self, cxn=None, **kwargs):
-        DG4xxxProxy.__init__(self, cxn=cxn, **kwargs)
+        DG4000Proxy.__init__(self, cxn=cxn, **kwargs)
