d4586c469e5b3ac63b02de25de48d084e6efef4b
SrQ <yesrgang@gmail.com>
2019-06-13 03:15:57 -0600

    6dds new style

---
+++ rf2/devices/hodt_aom.py
@@ -0,0 +1,15 @@
+from rf2.devices.ad9854 import AD9854, AD9854Proxy
+
+class Device(AD9854):
+    _serial_port = '/dev/ttyACM649383339323514011E0'
+    _arduino_address = 3
+
+    def _setup(self):
+        self.amplitude = 1.0
+        self.frequency = 30e6
+
+class DeviceProxy(Device, AD9854Proxy):
+    _serial_servername = 'yesr10_serial'
+
+    def __init__(self, **kwargs):
+        AD9854Proxy.__init__(self, **kwargs)
