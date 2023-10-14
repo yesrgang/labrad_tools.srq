cd5ed1be10a4dd291159dae09a2cb8166aeacabb
yesrgang <yesrgang@gmail.com>
2023-10-13 18:04:38 -0600

    Added ipython and fixed errors in blue slaves script

--- current_controller3/init_blue_slaves_console.py
+++ current_controller3/init_blue_slaves_console.py
@@ -3,6 +3,6 @@ rm = visa.ResourceManager()
 rmlist = rm.list_resources()
 f = rm.open_resource(rmlist[-1])
 
-print()
+print('')
 print('Current settings of blue slaves:')
-print('Usage: f.write(':SLOT <channel>;:ILD:SET .<current_in_mA>')
+print('Usage: f.write(\':SLOT <channel>;:ILD:SET .<current_in_mA>\')')
