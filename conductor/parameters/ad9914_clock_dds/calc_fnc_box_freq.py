1c12ebccd236bcf5eca8ed12b44dcfdcc9b4dbad
SrQ <yesrgang@gmail.com>
2024-02-10 03:00:01 -0700

    automatic backup commit

--- conductor/parameters/ad9914_clock_dds/calc_fnc_box_freq.py
+++ conductor/parameters/ad9914_clock_dds/calc_fnc_box_freq.py
@@ -1,4 +1,3 @@
-from conductor.parameter import ConductorParameter
 
 def calc_sr2_fnc_box_ref_freq(mjm_comb_demod, drive_freq):
     if mjm_comb_demod is None:
@@ -34,4 +33,15 @@ def calc_sr2_fnc_box_ref_freq(mjm_comb_demod, drive_freq):
     srq_steer = srq_dist_aom + srq_table_offset_aom - srq_dist_fnc
     
     srq_dist_fnc_demod = -2 * srq_dist_fnc
-    return float(srq_dist_fnc_demod)
+
+    f_0 = -235.610600e6 + 110.6 #20240209 calibration
+    delta_p = 1.431e3
+    f_pump = f_0 - delta_p
+    f_aom3 = 39.9642716e6 
+    f_offset = f_aom3 - f_pump
+    output_f_if_directDriveAOM3 = drive_freq + f_offset
+    return float(output_f_if_directDriveAOM3)
+
+    # retrurn float(srq_dist_fnc_demod) when the DDS goes to Christian's box
+#    return float(srq_dist_fnc_demod)
+
