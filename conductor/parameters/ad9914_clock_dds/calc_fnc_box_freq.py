3c53fd378b5741a919414a1ccb759e2909a1289b
SrQ <yesrgang@gmail.com>
2024-02-20 03:00:02 -0700

    automatic backup commit

--- conductor/parameters/ad9914_clock_dds/calc_fnc_box_freq.py
+++ conductor/parameters/ad9914_clock_dds/calc_fnc_box_freq.py
@@ -2,6 +2,7 @@
 def calc_sr2_fnc_box_ref_freq(mjm_comb_demod, drive_freq):
     if mjm_comb_demod is None:
         return
+   # print('!! DDS MJM_comb_demod!!', mjm_comb_demod)
     
     # aom on comb table. 0th order goes to comb. double passed, +1 order 
     # goes back through fiber.
@@ -34,12 +35,19 @@ def calc_sr2_fnc_box_ref_freq(mjm_comb_demod, drive_freq):
     
     srq_dist_fnc_demod = -2 * srq_dist_fnc
 
-    f_0 = -235.610600e6 + 110.6 #20240209 calibration
-    delta_p = 1.431e3
-    f_pump = f_0 - delta_p
-    f_aom3 = 39.9642716e6 
-    f_offset = f_aom3 - f_pump
-    output_f_if_directDriveAOM3 = drive_freq + f_offset
+    #f_0 = -235.610600e6 + 110.6 #20240209 calibration
+    #delta_p = 1.431e3
+    #f_pump = f_0 - delta_p
+    #f_aom3 = 39.9642716e6 
+    #f_offset = f_aom3 - f_pump
+    #output_f_if_directDriveAOM3 = drive_freq + f_offset
+
+    f_cleanup = 39.953090e6
+    f_m_srq_steer = 39.963090e6
+    f_offset = f_cleanup - f_m_srq_steer # 20240219: this 10 kHz offset is due to the fact that srq_dist_aom = -77.76e6 instead of -77.77e6 if you read the number on the Tektronik FG
+    output_f_if_directDriveAOM3 =  -srq_steer + f_offset
+
+    #return float(-srq_steer)
     return float(output_f_if_directDriveAOM3)
 
     # retrurn float(srq_dist_fnc_demod) when the DDS goes to Christian's box
