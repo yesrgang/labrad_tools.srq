18538d2985f5d753d43f314228851599f95f40b2
SrQ <yesrgang@gmail.com>
2024-04-27 03:00:02 -0600

    automatic backup commit

---
+++ conductor/parameters/ad9914_clock_dds/dds_amplitude_3.py
@@ -0,0 +1,22 @@
+from conductor.parameter import ConductorParameter
+
+class DDS_Amplitude_3(ConductorParameter):
+    """ 
+    Update amplitude of DDS profile 0 (in [0,1])
+    """
+
+    priority = 14
+    autostart = False
+    #value_type = 'list'
+    #value_type = 'data'
+    
+    def initialize(self,config):
+	self.connect_to_labrad()
+
+    def update(self):
+        if self.value is not None:
+
+            print('AD9914 amplitude 3:', self.value)
+            self.cxn.yesr14_ad9914.save_new_amplitude(3, self.value)
+
+Parameter = DDS_Amplitude_3
---
+++ conductor/parameters/ad9914_clock_dds/dds_frequency_3.py
@@ -0,0 +1,30 @@
+from conductor.parameter import ConductorParameter
+import json
+import calc_fnc_box_freq as fnc
+
+class DDS_Frequency_3(ConductorParameter):
+    """ 
+    Update amplitude of DDS profile 0 (in [0,1])
+    """
+
+    priority = 15
+    autostart = False
+    #value_type = 'list'
+    #value_type = 'data'
+    
+    def initialize(self,config):
+	self.connect_to_labrad()
+    def update(self):
+        if self.value is not None:
+            # calculate ref frequency for fnc box
+            mjm_comb_demod = json.loads(self.cxn.conductor.get_parameter_values(json.dumps({'si21.mjm_comb_demod_frequency':{}})))['si21.mjm_comb_demod_frequency']
+            if mjm_comb_demod is None:
+                return
+
+            out_freq = fnc.calc_sr2_fnc_box_ref_freq(mjm_comb_demod, self.value)
+            print('AD9914 frequency 3:', out_freq)
+            self.cxn.yesr14_ad9914.clear_programmable_modulus_mode()
+            self.cxn.yesr14_ad9914.save_new_frequency(3, out_freq)
+
+ 
+Parameter = DDS_Frequency_3
