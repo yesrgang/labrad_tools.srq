a96cc03757ff8aa40b7ddf9e9d6a5d2029e53eab
Stefan Lannig <stefan.lannig@gmail.com>
2024-11-14 15:59:33 -0700

    Added frequency value for DDS noise testing

--- fpga_dds/low_level_dds_tests/test_program_dds.py
+++ fpga_dds/low_level_dds_tests/test_program_dds.py
@@ -53,7 +53,8 @@ sock.settimeout(timeout)
 
 # constant output with the following parameters
 #ftw = 0x155555555  # 36-bit FTW
-ftw = 0x111111111  # 36-bit FTW
+#ftw = 0x111111111  # 36-bit FTW (10MHz)
+ftw = 0x444444444  # 36-bit FTW (40MHz)
 #ftw = 0x444444444  # 36-bit FTW
 atw = 0xff  # 8-bit ATW
 ptw = 0x0000 # 16-bit PTW
