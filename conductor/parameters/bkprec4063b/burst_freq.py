9b5cfccafcbdb38a2c3db43650ea71393aee78ee
SrQ <yesrgang@gmail.com>
2025-03-13 03:00:01 -0600

    automatic backup commit

--- conductor/parameters/bkprec4063b/burst_freq.py
+++ conductor/parameters/bkprec4063b/burst_freq.py
@@ -29,6 +29,7 @@ class Burst_Params(ConductorParameter):
         if self.value is not None:
             if (self.oldval is None) or (self.value != self.oldval):
                 #self.oldval = self.value
+                ampl = json.loads(self.cxn.conductor.get_parameter_values(json.dumps({'bkprec4063b.ampl':{}})))['bkprec4063b.ampl']
                 try:
                     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                     s.connect(self._bkprec_conn_details)
@@ -39,22 +40,22 @@ class Burst_Params(ConductorParameter):
                     dev = bkp.BKPrec4063B(s)
                     dev.send_cmd(b'C2:OUTP OFF')
 
-                    # sine wave
-                    dev.send_cmd(b'C2:BSWV WVTP,SINE')
-                    #dev.send_cmd(b'C2:BSWV FRQ,10000Hz')
-                    dev.send_cmd(b'C2:BSWV FRQ,{:.6f}Hz'.format(self.value))
-                    dev.send_cmd(b'C2:BSWV AMP,1Vpp')
-                    dev.send_cmd(b'C2:BSWV OFST,0V')
-                    dev.send_cmd(b'C2:BSWV PHSE,0')
+                    if ampl > 0:
+                        # sine wave
+                        dev.send_cmd(b'C2:BSWV WVTP,SINE')
+                        dev.send_cmd(b'C2:BSWV FRQ,{:.6f}Hz'.format(self.value))
+                        dev.send_cmd(b'C2:BSWV AMP,{:.4f}Vpp'.format(ampl))
+                        dev.send_cmd(b'C2:BSWV OFST,0V')
+                        dev.send_cmd(b'C2:BSWV PHSE,0')
 
-                    # gated burst
-                    dev.send_cmd(b'C2:BTWV STATE,ON')
-                    dev.send_cmd(b'C2:BTWV GATE_NCYC,GATE')
-                    dev.send_cmd(b'C2:BTWV TRSR,EXT')
-                    dev.send_cmd(b'C2:BTWV PLRT,POS')
-                    dev.send_cmd(b'C2:BTWV STPS,0')
+                        # gated burst
+                        dev.send_cmd(b'C2:BTWV STATE,ON')
+                        dev.send_cmd(b'C2:BTWV GATE_NCYC,GATE')
+                        dev.send_cmd(b'C2:BTWV TRSR,EXT')
+                        dev.send_cmd(b'C2:BTWV PLRT,POS')
+                        dev.send_cmd(b'C2:BTWV STPS,0')
 
-                    dev.send_cmd(b'C2:OUTP ON,HZ')
+                        dev.send_cmd(b'C2:OUTP ON,HZ')
                 except Exception as e:
                     print(e)
                     s.close()
