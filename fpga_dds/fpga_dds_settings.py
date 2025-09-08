cc6fac88446d00f94cca409f55305e34ab58d1ea
srgang <yesrgang@gmail.com>
2025-09-08 16:30:33 -0600

    Modify FPGA-DDS frequency limits to represent actual values of updated FPGA firmware

--- fpga_dds/fpga_dds_settings.py
+++ fpga_dds/fpga_dds_settings.py
@@ -5,12 +5,12 @@ N_DIGITAL = 7       # number of TTL output channels
 
 # timing
 T_BITS = 48
-T_MIN = 1/150e6
+T_MIN = 1/300e6
 T_MAX = 2**T_BITS * T_MIN
 
 # frequency
 F_BITS = 36
-F_MAX = 150e6
+F_MAX = 300e6
 
 # amplitude
 A_BITS = 8
